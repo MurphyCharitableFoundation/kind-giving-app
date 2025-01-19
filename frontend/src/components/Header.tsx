53028fa3c477ff6d15fd47f37c21067dcdd24c53
Renato Guedes <105191060+renatoguedes-dev@users.noreply.github.com>
2025-01-19 10:55:19 -0300

    feat: create initial homepage structure with header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex-row align-center" style={{ justifyContent: "space-between"}}>
+      <Link to="/">
+        <h1>Kind Giving App</h1>
+      </Link>
+
+      <nav>
+        <ul className="flex-row gap20">
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+
+          <Link to="/signup">
+            <li>Signup</li>
+          </Link>
+
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+
+          <Link to="/faq">
+            <li>FAQ</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
