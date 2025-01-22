071306296d848fea05e988f98e67dea4994cf9e4
Renato Guedes <105191060+renatoguedes-dev@users.noreply.github.com>
2025-01-21 23:53:33 -0300

    feat: create NotFound page to catch all non-existing pages

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,9 +1,11 @@
-import App from "./pages/App";
+import App from "./pages/Home/App";
+import NotFoundPage from "./pages/NotFound/NotFound";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFoundPage />,
   },
 ];
 
