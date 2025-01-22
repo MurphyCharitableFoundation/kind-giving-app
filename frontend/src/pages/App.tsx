123971775c79d524ec29afcd6ecdd7d9e37f000e
Renato Guedes <105191060+renatoguedes-dev@users.noreply.github.com>
2025-01-21 21:59:49 -0300

    Update Docker files related to frontend (#27)
    * feat: create initial homepage structure with header
    
    * chore: update configurations for the frontend on docker-compose.yml and image tag and unnecessary commands on Dockerfile

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <Container sx={{ height: "100vh", marginTop: "10px" }}>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
