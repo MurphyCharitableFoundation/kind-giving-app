468b7b8124c0875ae18f9f71568cd9efa76332b7
Ayo Onipe <ayosemail@gmail.com>
2025-01-23 08:37:45 -0700

    Replace phonenumber_field on User & UserGroups

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third Party Apps:
+    "phonenumber_field",
     # Project Apps:
     "core",
     "user",
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,27 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 from .managers import UserManager
 
 
 class UserGroup(models.Model):
     name = models.CharField(max_length=255)
-    phone_number = models.CharField(
-        max_length=15,  # Allows for international numbers
-        validators=[
-            RegexValidator(
-                regex=r"^\+?1?\d{9,15}$",
-                message=(
-                    "Phone number must be entered in the format: '+999999999'. "
-                    "Up to 15 digits allowed."
-                ),
-            )
-        ],
-        blank=False,
-        unique=True,
-    )
+    phone_number = PhoneNumberField(blank=True, null=True)
     interest = models.CharField(max_length=255, blank=True, null=True)
     img = models.ImageField(
         upload_to="usergroup_images/", blank=True, null=True
@@ -29,26 +16,13 @@ class UserGroup(models.Model):
     bank_account = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"UserGroup: {self.name}"
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    phone_number = models.CharField(
-        max_length=15,  # Allows for international numbers
-        validators=[
-            RegexValidator(
-                regex=r"^\+?1?\d{9,15}$",
-                message=(
-                    "Phone number must be entered in the format: '+999999999'. "
-                    "Up to 15 digits allowed."
-                ),
-            )
-        ],
-        blank=False,
-        unique=True,
-    )
+    phone_number = PhoneNumberField(blank=True, null=True)
     group_membership = models.ForeignKey(
         UserGroup,
         on_delete=models.SET_NULL,
@@ -66,4 +40,4 @@ class User(AbstractUser):
     objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return f"User: {self.email}"
